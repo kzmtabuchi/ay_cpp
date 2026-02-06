39f6940a344961fdf75727d910d5c93bd867f5e7
kzmtabuchi <tabuchi.kazuma@fingervision.biz>
2026-02-06 02:10:40 +0000

    Add the option to run from ros2 launch

--- test/launch/test_rviz_util.launch.py
+++ test/launch/test_rviz_util.launch.py
@@ -6,17 +6,20 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_test_description():
+
+def get_launch_description():
     pkg_path = get_package_share_directory('ay_cpp')
     rviz_config_path = os.path.join(pkg_path, "test", "rviz", "test.rviz")
-    
+
     rviz_node = Node(
-        package='rviz2', executable='rviz2',
+        package='rviz2',
+        executable='rviz2',
         arguments=['-d', rviz_config_path],
         output='screen'
     )
     gtest_node = Node(
-        package='ay_cpp', executable='test_ay_cpp_rviz',
+        package='ay_cpp',
+        executable='test_ay_cpp_rviz',
         output='screen'
     )
 
@@ -24,13 +27,29 @@ def generate_test_description():
         rviz_node,
         gtest_node,
         launch_testing.actions.ReadyToTest(),
-    ]), {'gtest': gtest_node}
+    ]), gtest_node
 
-# unittest 形式のテストクラス
+
+def generate_launch_description():
+    # ros2 launch が呼ぶ関数
+
+    ld, _ = get_launch_description()
+    return ld
+
+
+def generate_test_description():
+    # colcon test (launch_testing) が呼ぶ関数
+
+    ld, gtest = get_launch_description()
+    return ld, {'gtest': gtest}
+
+
 class TestGtestBasic(unittest.TestCase):
+    # unittest 形式のテストクラス
     def test_gtest_run(self, proc_info, gtest):
-        # C++側の終了（sleep 10s含む）まで最大60秒待機
+        # C++側の終了まで最大60秒待機
         proc_info.assertWaitForShutdown(process=gtest, timeout=60)
+
 
 @launch_testing.post_shutdown_test()
 class TestShutdown(unittest.TestCase):
